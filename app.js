a32070dae22f5f26472df33a24161e277ce52de1
RodriGBorges <gabii.borges1998@gmail.com>
2021-08-08 17:03:12 -0300

    Base de Mercado Liebre terminada.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+app.use(express.static('public'));
+
+app.get('/home', (req , res) => {res.sendFile(path.join(__dirname, 'views', 'home.html'))});
+
+app.listen(port, () => console.log('Server listening on http://localhost:' + port + '/home'));
+
+
