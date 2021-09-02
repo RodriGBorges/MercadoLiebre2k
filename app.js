dedeb135d5db6d99fc51a32d120594381cff9f50
RodriGBorges <gabii.borges1998@gmail.com>
2021-09-02 19:38:48 -0300

    Vista Register y Login terminadas

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get('/home', (req , res) => {res.sendFile(path.join(__dirname, 'views', 'home.html'))});
+app.get('/register', (req , res) => {res.sendFile(path.join(__dirname, 'views', 'register.html'))});
+app.get('/login', (req , res) => {res.sendFile(path.join(__dirname, 'views', 'login.html'))});
 
 app.listen(port, () => {console.log('Server listening on http://localhost:' + port + '/home')});
 
